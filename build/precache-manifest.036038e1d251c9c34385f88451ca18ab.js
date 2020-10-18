758aa1e45d47198f03b34942d82b63bfbd6a50d5
berbersky <60584511+berbersky@users.noreply.github.com>
2020-10-18 08:45:38 +0100

    update

---
+++ build/precache-manifest.036038e1d251c9c34385f88451ca18ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b5a7293b75364c35cb5d685ecf75e0",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "057945bcda2607b4279c",
+    "url": "/Monsters/static/css/main.7b305927.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Monsters/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057945bcda2607b4279c",
+    "url": "/Monsters/static/js/main.9fd16716.chunk.js"
+  },
+  {
+    "revision": "693fc5d752b245bbfa88",
+    "url": "/Monsters/static/js/runtime-main.ce3081d1.js"
+  }
+]);
